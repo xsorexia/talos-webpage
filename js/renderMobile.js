a8c679a9879203e0cd74748d0d3d66e7fd80dab1
xsorexia <xsorexia@gmail.com>
2025-01-12 23:54:47 +0900

    render

--- js/renderMobile.js
+++ js/renderMobile.js
@@ -1,4 +1,4 @@
-// JavaScript - /js/renderMobile.js
+// JavaScript - /js/renderMobile.js?ver=1
 
     var curState = "desktop";
     var isDesktopLoaded = false;
@@ -37,3 +37,35 @@
                 $(".mobile-css").attr("disabled", "disabled");
         }
     });
+
+
+    var isUniqueVisit = 0
+    if (sessionStorage.getItem("xso-visit") != "visit") {
+        isUniqueVisit = 1
+    } 
+    sessionStorage.setItem("xso-visit", "visit")
+    
+    var domainSplit = window.location.host.split(".");
+    var domain = ""
+    for (var i = 0; i < domainSplit.length; i++) {
+        if (domainSplit[i] != "www") {
+            if (domain != "") {
+                domain += "."
+            }
+            domain += domainSplit[i];
+        }
+    }
+    const data = {
+        "isUniqueVisit": isUniqueVisit,
+        "domain": domain,
+        "location": window.location.pathname
+    };
+    fetch("https://visitors.xsorexia.com/api/visitPage.php", {
+        method: "POST",  // HTTP method
+        headers: {
+            "Content-Type": "application/json"  // Specify JSON format
+        },
+    
+        body: JSON.stringify(data)  // Convert the data object to a JSON string
+    })
+    
